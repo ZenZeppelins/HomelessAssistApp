8931eb7b3a466368dc98c4eecbd365b94feb319a
Mayhul Arora <mayhul99@gmail.com>
2018-03-24 19:58:16 -0400

    Map now loads all active shelters

--- app/src/main/java/zepplins/zen/homelessassist/controllers/MapActivity.java
+++ app/src/main/java/zepplins/zen/homelessassist/controllers/MapActivity.java
@@ -1,7 +1,11 @@
 package zepplins.zen.homelessassist.controllers;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,7 +14,11 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.List;
+
 import zepplins.zen.homelessassist.R;
+import zepplins.zen.homelessassist.model.Model;
+import zepplins.zen.homelessassist.model.Shelter;
 
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -24,25 +32,52 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.searchMapFab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), SearchActivity.class);
+                i.putExtra("source", "map");
+                startActivity(i);
+            }
+        });
     }
 
 
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
+     * Load all the pins that are active
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        List<Shelter> active = Model.getInstance().getActiveShelters();
+        double latTotal = 0;
+        double longTotal = 0;
+        int count = 0;
+        //For each shelter, load a marker at the location with the corresponding info
+        for (Shelter s : active) {
+            LatLng loc = new LatLng(s.getLatitude(), s.getLongitude());
+            String snippet = "Phone: " + s.getPhoneNumber();
+            mMap.addMarker(new MarkerOptions().position(loc).title(s.getShelterName()).snippet(snippet));
+            latTotal += s.getLatitude();
+            longTotal += s.getLongitude();
+            count++;
+        }
+        //Calculate the average latitute and longitude
+        double latAvg = latTotal / count;
+        double longAvg = longTotal / count;
+        //If no shelters are in active, set camera to Georgia Tech
+        if (active.size() == 0) {
+            latAvg = 33.7756;
+            longAvg = -84.3963;
+        }
+        //Move camera to average lat/long of shelters
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(latAvg, longAvg)));
+        //11 found through experimentation
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(11));
     }
 }
